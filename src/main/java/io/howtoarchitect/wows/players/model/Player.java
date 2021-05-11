d747e418e53829b4b7b4e290a440dba2c548c8c6
Kapil Viren Ahuja <k.v.ahuja@gmail.com>
2021-05-11 20:52:25 +0530

    Initial Spring boot commig

---
+++ src/main/java/io/howtoarchitect/wows/players/model/Player.java
@@ -0,0 +1,114 @@
+package io.howtoarchitect.wows.players.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "players")
+public class Player {
+    private long id;
+    private String nickname;
+    private String region;
+    private long levellingTier;
+    private long levellingPoints;
+    private Boolean hiddenProfile;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date logoutAt;
+    private Date statsUpdatedAt;
+
+    @Id
+    @Column(name = "application_id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(length = 25)
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Column(length = 3)
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public long getLevellingTier() {
+        return levellingTier;
+    }
+
+    public void setLevellingTier(long levellingTier) {
+        this.levellingTier = levellingTier;
+    }
+
+    public long getLevellingPoints() {
+        return levellingPoints;
+    }
+
+    public void setLevellingPoints(long levellingPoints) {
+        this.levellingPoints = levellingPoints;
+    }
+
+    public Boolean getHiddenProfile() {
+        return hiddenProfile;
+    }
+
+    public void setHiddenProfile(Boolean hiddenProfile) {
+        this.hiddenProfile = hiddenProfile;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getLogoutAt() {
+        return logoutAt;
+    }
+
+    public void setLogoutAt(Date logoutAt) {
+        this.logoutAt = logoutAt;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getStatsUpdatedAt() {
+        return statsUpdatedAt;
+    }
+
+    public void setStatsUpdatedAt(Date statsUpdatedAt) {
+        this.statsUpdatedAt = statsUpdatedAt;
+    }
+
+}
---
+++ src/test/java/io/howtoarchitect/wows/players/PlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.howtoarchitect.wows.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
