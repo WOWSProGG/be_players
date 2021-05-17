c53fa14fe0378e3bc9934f159277460973b71eae
Kapil Viren Ahuja <k.v.ahuja@gmail.com>
2021-05-18 02:50:33 +0530

    sonar fixed - code coverage
    tests to check...
     - spring application is loaded
     - we are able to make api call

--- src/test/java/io/howtoarchitect/wows/players/PlayersApplicationTests.java
+++ src/test/java/io/howtoarchitect/wows/players/PlayersApplicationTests.java
@@ -1,13 +1,34 @@
 package io.howtoarchitect.wows.players;
 
+import io.howtoarchitect.wows.players.controller.PlayerController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PlayersApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private PlayerController controller;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	void makePlayerAPICAll() {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/players/NukeDuckSr", String.class)).isNotNull();
 	}
 
 }
